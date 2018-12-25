2b0dd4b3a04d9baf447051889766db11a58515e9
Fraz Ahsan <fraz.ahsan17@gmail.com>
2018-12-25 12:31:54 +0500

    Dashboard page added for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'admins'
   namespace :admin do
-    get 'dashboard/index'
+    get 'dashboard' => 'dashboard#index'
   end
   post '/recognize' => 'welcome#recognize'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
