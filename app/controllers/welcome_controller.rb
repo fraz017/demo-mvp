6ddc94c34078e58c47f410396e3d3ee110dd803f
Fraz Ahsan <fraz.ahsan17@gmail.com>
2018-12-28 17:07:54 +0500

    Content Url added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -44,6 +44,8 @@ class WelcomeController < ApplicationController
       found = find(image)
       if found
         @track.create(device_id: params[:id])
+        url = Content.first&.url
+        url = "http://layslanded.visidots.com" if url.nil?
         render json: {url: "http://layslanded.visidots.com"}
       else
         render json: {msg: "<strong>Logo</strong> not recognized. Please try again!"}
